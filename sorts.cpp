40081af85f00f0fe8a819d367a1601cfb1caf063
Akir321 <anton-1945@bk.ru>
2023-09-14 22:24:11 +0300

    better sorting
    now comparisons ignore everyything that is not letters

--- sorts.cpp
+++ sorts.cpp
@@ -13,10 +13,15 @@ int myStrCmp(const void *s1, const void *s2)
     const char *str1 = ((const string *)s1)->str;
     const char *str2 = ((const string *)s2)->str;
 
+    while (!isalpha(*str1) && *str1 != '\0' && *str1 != '\n') { str1++; }
+    while (!isalpha(*str2) && *str2 != '\0' && *str2 != '\n') { str2++; }
+
     while (*str1 == *str2 && *str1 != '\0' && *str1 != '\n')
     {
         str1++;
         str2++;
+        while (!isalpha(*str1) && *str1 != '\0' && *str1 != '\n') { str1++; }
+        while (!isalpha(*str2) && *str2 != '\0' && *str2 != '\n') { str2++; }
     }
     return *str1 - *str2;
 }
@@ -32,10 +37,15 @@ int myStrBackCmp(const void *s1, const void *s2)
     long long i1 = (long long)(((const string *)s1)->size - 1);
     long long i2 = (long long)(((const string *)s2)->size - 1);
 
+    while (!isalpha(str1[i1]) && i1 >= 0) { i1--; }
+    while (!isalpha(str2[i2]) && i2 >= 0) { i2--; }
+
     while (str1[i1] == str2[i2] && i1 >= 0 && i2 >= 0)
     {
         i1--;
         i2--;
+        while (!isalpha(str1[i1]) && i1 >= 0) { i1--; }
+        while (!isalpha(str2[i2]) && i2 >= 0) { i2--; }
     }
     return str1[i1] - str2[i2];
 }
