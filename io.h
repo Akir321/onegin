a820acb476ae26c0befd5770df8dc1cb76d867fd
Akir321 <anton-1945@bk.ru>
2023-09-22 14:50:10 +0300

    doxygen documentation

--- io.h
+++ io.h
@@ -5,12 +5,20 @@
 
 const int NAME_ADD_LENGTH = 7;
 
+/**
+ * @brief a structure to keep a string pointer and str length
+ * 
+ */
 struct str
 {
     char *str;
     size_t size;
 };
 
+/**
+ * @brief a structure that contains all the text parameters
+ * 
+ */
 struct textArray 
 {
     size_t size;
@@ -20,34 +28,106 @@ struct textArray
     str *strings;
 };
 
+/**
+ * @brief enum of errors with text
+ * 
+ */
 enum errors
 {
     FILE_ERROR = 1,
     MEMORY_ERROR = 2
 };
 
+/**
+ * @brief destructor for struct textArray
+ * 
+ * @param [in] text 
+ */
 void destroyTextArray(textArray *text);
 
+/**
+ * @brief returns the file size from struct stat
+ * 
+ * @param [in] name 
+ * @return int
+ */
 int fileSize(const char *name);
 
+/**
+ * @brief a constructor for struct textArray
+ * 
+ *      reads the text to a buffer and splits it to strings with char *
+ * 
+ * @param [in] fileName 
+ * @param [in, out] text 
+ * @return int 
+ */
 int readTextFromFile(const char *fileName, textArray *text);
 
+/**
+ * @brief reads text from FILE *f into a buffer, returns the buffer
+ * 
+ * @param [in] f 
+ * @param [in] size 
+ * @return char* buffer 
+ */
 char *readToBuffer(FILE *f, size_t size);
 
+/**
+ * @brief prints out size bytes of str with their codes
+ * 
+ * @param [in] str 
+ * @param [in] size 
+ */
 void printStr(char *str, size_t size);
 
+/**
+ * @brief splits buffer from textArray to strings with char *
+ * 
+ * @param [in, out] text 
+ * @return 0 if no error encountered
+ * @return positive int - errors enum
+ */
 int splitBufToStrings(textArray *text);
 
+/**
+ * @brief count the amount of "c" symbols in "buffer"
+ * 
+ * @param [in] buffer 
+ * @param [in] c 
+ * @return size_t 
+ */
 size_t countChar(const char *buffer, char c);
 
+/**
+ * @brief writes the text (from str *strings) to stdout
+ * 
+ * @param [in] text 
+ * @param [in] nLines 
+ */
 void printText(str *text, size_t nLines);
 
+/**
+ * @brief puts that works with '\\n' like with '\0'
+ * 
+ * @param [in] str 
+ */
 void myPuts(const char *str);
 
+/**
+ * @brief fputs that works with '\\n' like with '\0'
+ * 
+ * @param [in] str 
+ * @param [in] f  file to write to
+ */
 void myFPuts(const char *str, FILE *f);
 
+/**
+ * @brief writes the text (from str *strings) to file
+ * 
+ * @param [in] text 
+ * @param [in] f 
+ */
 void printTextToFile(const textArray *text, FILE *f);
-
-size_t min(size_t a, size_t b);
 
 #endif // IO_H
--- sorts.h
+++ sorts.h
@@ -4,14 +4,68 @@
 #include <stdio.h>
 #include "io.h"
 
+/**
+ * @brief compares the strings in lexicographical order
+ * 
+ * takes into account only letters, upper- and lowercase is considered equal
+ * 
+ * @param [in] s1 
+ * @param [in] s2 
+ * @return negative int if s1 < s2
+ * @return 0 if s1 = s2
+ * @return positive int if s1 > s2 
+ */
 int myStrCmp(const void *s1, const void *s2);
 
+/**
+ * @brief compares the strings in lexicographical order starting from the end of the string
+ * 
+ * takes into account only letters, upper- and lowercase is considered equal
+ * 
+ * @param [in] s1 
+ * @param [in] s2 
+ * @return negative int if s1 < s2
+ * @return 0 if s1 = s2
+ * @return positive int if s1 > s2 
+ */
 int myStrBackCmp(const void *s1, const void *s2);
 
+/**
+ * @brief qsort realisation for struct str type
+ * 
+ * @param [in, out] array 
+ * @param [in] left 
+ * @param [in] right 
+ * @param [in] comp 
+ */
 void quickSort  (str *array, size_t left, size_t right, int (*comp)(const str *, const str *));
 
+/**
+ * @brief a function that changes the array in qsort and returns mid index
+ * 
+ * @param [in, out] array 
+ * @param [in] left 
+ * @param [in] right 
+ * @param [in] comp 
+ * @return size_t mid - the border where to partition the array 
+ */
 size_t partition(str *array, size_t left, size_t right, int (*comp)(const str *, const str *));
 
+/**
+ * @brief swaps the two struct str variables
+ * 
+ * @param [in, out] a 
+ * @param [in, out] b 
+ */
 void swap(str *a, str *b);
+
+/**
+ * @brief returns minimum of two size_t variables
+ * 
+ * @param [in] a 
+ * @param [in] b 
+ * @return size_t 
+ */
+size_t min(size_t a, size_t b);
 
 #endif //SORTS_H
